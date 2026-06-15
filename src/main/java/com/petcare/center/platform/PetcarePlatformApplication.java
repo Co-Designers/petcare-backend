879aae5ec83faf89dbc08d91b5ad738a8ef2c52f
Ghiou Justinn Mauricio Silva <gjustinnmauricios001@gmail.com>
2026-06-15 15:26:40 -0500

    chore: add PetCare platform application entry point

---
+++ src/main/java/com/petcare/center/platform/PetcarePlatformApplication.java
@@ -0,0 +1,11 @@
+package com.petcare.center.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcarePlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PetcarePlatformApplication.class, args);
+    }
+}
